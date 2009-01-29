3871818888ef8a7a410582829cac9eb07969421e
Justin Tomich <tomichj@bacon.local>
2009-01-28 18:43:16 -0800

    just nuking dumb default IDEA headers

--- src/idea/conf/build/ModuleLibraryContainer.groovy
+++ src/idea/conf/build/ModuleLibraryContainer.groovy
@@ -2,13 +2,6 @@ package idea.conf.build
 
 import idea.conf.Visitable
 
-/**
- * Created by IntelliJ IDEA.
- * User: tomichj
- * Date: Jan 6, 2009
- * Time: 2:26:31 PM
- * To change this template use File | Settings | File Templates.
- */
 
 public class ModuleLibraryContainer implements Visitable
 {
