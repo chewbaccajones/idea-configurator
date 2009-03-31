fc9231044140227e1e7a96fc339594c41e6fdc1c
Justin Tomich <tomichj@ham.local>
2009-03-31 16:20:45 -0700

    more not-working testing

--- test/idea/conf/java/depend/SrcTest.groovy
+++ test/idea/conf/java/depend/SrcTest.groovy
@@ -6,17 +6,24 @@ import org.apache.tools.ant.BuildFileTest
  * 
  */
 
-public class SrcTest //extends BuildFileTest
+// problem, ant's BuildFileTest is a layer up in the classloader heirarchy, fails
+// without junit in ant.lib.dir
+public class SrcTest extends BuildFileTest
 {
-    public void testNothing() { }
+    public void testNothing()
+    {
+        
+    }
     
-    //public void setUp() {
-    //    // initialize Ant
-    //    configureProject("build-test.xml");
-    //}
-    //
-    //void testEmpty()
-    //{
-    //    executeTarget("empty");
-    //}
+    public void setUp()
+    {
+        // initialize Ant
+        configureProject("build-test.xml");
+    }
+
+    void testEmpty()
+    {
+        executeTarget("empty");
+        assertLogContaining("foo")
+    }
 }
